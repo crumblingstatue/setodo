418bdbf19437d2f56b0bf9c0156edd8a1b70cc88
crumblingstatue <radiantstatue@gmail.com>
2024-03-30 20:08:25 +0100

    Restore window decorations
    Being able to move/resize the window easily is worth it over
    not allowing to minimize due to a bug.

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,7 @@ fn main() {
         }
     };
     let native_options = eframe::NativeOptions {
-        viewport: ViewportBuilder::default()
-            .with_decorations(false)
-            .with_inner_size(egui::vec2(620., 480.)),
+        viewport: ViewportBuilder::default().with_inner_size(egui::vec2(620., 480.)),
         ..Default::default()
     };
     eframe::run_native(
